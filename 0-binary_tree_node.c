d12242eda1115a7735586238d69ef06e4acc400e
Majidied <majidimajidi2003@gmail.com>
2024-01-31 16:25:22 +0100

    task 0

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ *binary_tree_node - create a binary tree node
+ *@parent: the parent of this new node.
+ *@value: integer to store in the node.
+ *Return: pointer to the created node.
+*/
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	new = (struct binary_tree_s *)malloc(sizeof(struct binary_tree_s));
+	if (!new)
+		return (NULL);
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
+	new->n = value;
+	return (new);
+}
---
+++ Makefile
@@ -0,0 +1,17 @@
+CC = gcc
+CFLAGS = -Wall -Werror -Wextra -pedantic
+
+SRC_FILES = main.c 0-binary_tree_node.c binary_tree_print.c
+HEADER_FILE = binary_trees.h
+
+TARGET = binary_tree
+
+all: $(TARGET)
+
+$(TARGET): $(SRC_FILES)
+	$(CC) $(CFLAGS) -o $(TARGET) $(SRC_FILES)
+
+clean:
+	rm -f $(TARGET)
+
+re: clean all
