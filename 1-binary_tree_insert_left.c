ce45869c48b67a064bda5c635b59f732abb63238
Majidied <majidimajidi2003@gmail.com>
2024-01-31 16:47:40 +0100

    task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,6 +14,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 	new = binary_tree_node(parent, value);
+	if (new == NULL)
+		return (NULL);
 	if (parent->left != NULL)
 	{
 		temp = parent->left;
