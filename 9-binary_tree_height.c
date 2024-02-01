8cef86b33b9f7aa071ffafeecf1b1ed1d8af05a2
Majidied <majidimajidi2003@gmail.com>
2024-02-01 03:20:52 +0100

    task 9

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - height of a binary tree
+ * @tree: root node of the binary tree
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left, right;
+
+	if (tree == NULL || binary_tree_is_leaf(tree))
+		return (0);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	return (1 + ((left > right) ? left : right));
+}
