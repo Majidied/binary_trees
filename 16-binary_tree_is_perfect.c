cae1e89b1085557b73e4affe6eaaaa6aab133801
Majidied <majidimajidi2003@gmail.com>
2024-02-01 23:17:06 +0100

    task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -29,25 +29,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	return (1 + (left > right ? left : right));
 }
 
-
-/**
- * binary_tree_size - size of a binary tree
- * @tree: root node of the binary tree
- * Return: size of the binary tree or 0 if it's empty
- */
-
-size_t binary_tree_size(const binary_tree_t *tree)
-{
-	size_t left, right;
-
-	if (tree == NULL)
-		return (0);
-
-	left = binary_tree_size(tree->left);
-	right = binary_tree_size(tree->right);
-	return (1 + (left + right));
-}
-
 /**
  * binary_tree_is_perfect - returns whether a binary tree is perfect or not.
  * @tree: a binary tree to check
@@ -55,7 +36,6 @@ size_t binary_tree_size(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t height, nodes;
 
 	if (tree == NULL)
 		return (0);
