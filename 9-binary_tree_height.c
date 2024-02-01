156a5ad32ed28d4298b73d1bcb485ec9d8dbe511
Majidied <majidimajidi2003@gmail.com>
2024-02-01 20:46:59 +0100

    task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,5 +14,5 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
-	return (1 + ((left > right) ? left : right));
+	return (1 + (left > right ? left : right));
 }
