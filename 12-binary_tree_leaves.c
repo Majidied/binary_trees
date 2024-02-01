77fe157aaae42f2210a6b1cee23b859101920091
Majidied <majidimajidi2003@gmail.com>
2024-02-01 21:37:48 +0100

    task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,16 +1,16 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf - returns true if a node is a leaf, false otherwise.
+ * binary_tree_is_leaf1 - returns true if a node is a leaf, false otherwise.
  * @node: the node to be checked.
  * Return: 0 if the node has no children, 0 otherwise.
  */
 
 int binary_tree_is_leaf1(const binary_tree_t *node)
 {
-	if (node == NULL || !(node->left == NULL && node->right == NULL))
-		return (0);
-	return (1);
+	if (node != NULL && node->left == NULL && node->right == NULL)
+		return (1);
+	return (0);
 }
 
 /**
