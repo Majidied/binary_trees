120b6592c374b40228724ea557714404c9e644a6
Majidied <majidimajidi2003@gmail.com>
2024-02-01 21:03:09 +0100

    task 10

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - depth of a binary tree.
+ * @tree: the tree to get the depth from.
+ * Return: The depth of the tree, 0 if it's empty.
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth= 0;
+
+	if (tree == NULL || tree->parent == NULL)
+		return (0);
+
+	while (tree->parent != NULL)
+	{
+		tree  = tree->parent;
+		depth++;
+	}
+
+	return (depth);
+
+}
