f64badc9ca7d142ca3c1951921d67701a96411fb
Majidied <majidimajidi2003@gmail.com>
2024-02-01 21:40:53 +0100

    task 9 and task12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -24,13 +24,13 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	size_t left, right;
 
 	if (tree == NULL)
-    {
+	{
 		return (0);
-    }
-    if (binary_tree_is_leaf1(tree))
-    {
-        return(1);
-    }
+	}
+	if (binary_tree_is_leaf1(tree))
+	{
+		return(1);
+	}
 
 	left = binary_tree_leaves(tree->left);
 	right = binary_tree_leaves(tree->right);
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,5 +1,19 @@
 #include "binary_trees.h"
 
+
+/**
+ * binary_tree_is_leaf1 - returns true if a node is a leaf, false otherwise.
+ * @node: the node to be checked.
+ * Return: 0 if the node has no children, 0 otherwise.
+ */
+
+int binary_tree_is_leaf1(const binary_tree_t *node)
+{
+	if (node != NULL && node->left == NULL && node->right == NULL)
+		return (1);
+	return (0);
+}
+
 /**
  * binary_tree_height - height of a binary tree
  * @tree: root node of the binary tree
@@ -10,7 +24,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left, right;
 
-	if (tree == NULL || binary_tree_is_leaf(tree))
+	if (tree == NULL || binary_tree_is_leaf1(tree))
 		return (0);
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
