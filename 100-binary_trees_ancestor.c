e7b0e7e126337d1f26e0ee057402e910bfec5f64
Majidied <majidimajidi2003@gmail.com>
2024-02-02 02:57:38 +0100

    task 19

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -9,19 +9,16 @@
 
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
 {
-	if (first == NULL || second == NULL) {
-		return NULL;
-	}
-	if (first->parent == second)
-	{
-		return (first->parent);
-	}
-	else if (second->parent == first)
-	{
-		return (second->parent);
-	}
-	else
-	{
-		return ((first->parent == second->parent) ? first->parent : binary_trees_ancestor(first->parent->n > second->parent->n ? first : first->parent, second->parent->n > first->parent->n ? second : second->parent));
-	}
+    const binary_tree_t *temp_first, *temp_second;
+
+    if (first == NULL || second == NULL)
+        return NULL;
+    temp_first = (binary_tree_t *)first;
+    temp_second = (binary_tree_t *)second;
+    while (temp_first != temp_second)
+    {
+        temp_first = (temp_first == NULL) ? second : temp_first->parent;
+        temp_second = (temp_second == NULL) ? first : temp_second->parent;
+    }
+    return ((binary_tree_t *)temp_first);
 }
